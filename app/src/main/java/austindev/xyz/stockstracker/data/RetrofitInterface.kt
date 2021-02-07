8c2c1027db6f3b99306cd67720d74b0171314db7
Austin White <austin.j.white.96@gmail.com>
2021-02-06 20:57:06 -0800

    Update RetrofitInterface.kt

--- app/src/main/java/austindev/xyz/stockstracker/data/RetrofitInterface.kt
+++ app/src/main/java/austindev/xyz/stockstracker/data/RetrofitInterface.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Headers
 
 interface RetrofitInterface {
 
-    @Headers("x-rapidapi-key: 220eccff1emsh0077a33886d04fdp13d889jsna69e19283301", "x-rapidapi-host: morning-star.p.rapidapi.com")
+    @Headers("x-rapidapi-key:", "x-rapidapi-host:" )
     @GET("https://morning-star.p.rapidapi.com/market/v2/get-movers/")
     fun getGainers(): Call<List<Gainer?>?>?
 }
