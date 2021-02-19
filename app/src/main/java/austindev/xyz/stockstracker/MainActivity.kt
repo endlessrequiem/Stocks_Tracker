2fb812ee31f9d385fe0cfe2a549255ea2147cbc6
endlessrequiem <austin.j.white.96@gmail.com>
2021-02-18 19:09:41 -0800

    removed redundant shared preferences items in MainActivity.kt

--- app/src/main/java/austindev/xyz/stockstracker/MainActivity.kt
+++ app/src/main/java/austindev/xyz/stockstracker/MainActivity.kt
@@ -13,44 +13,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    var currTicker: SharedPreferences? = null
-    var currLastPrice: SharedPreferences? = null
-    var currPriceChange: SharedPreferences? = null
-    var currPercentageChange: SharedPreferences? = null
-    var currStanName: SharedPreferences? = null
-    var currExchangeName: SharedPreferences? = null
-    var currVolume: SharedPreferences? = null
-    var currPerformanceID: SharedPreferences? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        currTicker = getSharedPreferences(
-            getString(R.string.ticker_key), MODE_PRIVATE
-        )
-        currLastPrice = getSharedPreferences(
-            getString(R.string.lastPrice_key), MODE_PRIVATE
-        )
-        currPriceChange = getSharedPreferences(
-            getString(R.string.priceChange_key), MODE_PRIVATE
-        )
-        currPercentageChange = getSharedPreferences(
-            getString(R.string.percentageChange_key), MODE_PRIVATE
-        )
-        currStanName = getSharedPreferences(
-            getString(R.string.stanName_key), MODE_PRIVATE
-        )
-        currExchangeName = getSharedPreferences(
-            getString(R.string.exchangeStanName_key), MODE_PRIVATE
-        )
-        currVolume = getSharedPreferences(
-            getString(R.string.volume_key), MODE_PRIVATE
-        )
-        currPerformanceID = getSharedPreferences(
-            getString(R.string.performanceID_key), MODE_PRIVATE
-        )
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
